7c3f2a38c224a78131b1a9b83bd0f458ab0fb2cb
lndlazy <lndlazy@163.com>
2023-02-16 15:07:31 +0800

    日志信息

--- commonTest/src/main/java/com/lee/commontest/base/CommonApplication.kt
+++ commonTest/src/main/java/com/lee/commontest/base/CommonApplication.kt
@@ -22,12 +22,12 @@ class CommonApplication : Application() {
     private fun initARouter() {
 
         // 这两行必须写在init之前，否则这些配置在init过程中将无效
-        if (BuildConfig.DEBUG) {
+//        if (BuildConfig.DEBUG) {
             // 打印日志
             ARouter.openLog();
             // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
             ARouter.openDebug();
-        }
+//        }
         // 尽可能早，推荐在Application中初始化
         ARouter.init(this);
 
